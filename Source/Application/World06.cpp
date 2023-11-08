f1854c8c76af1993ea2b76d2eabc42e1b67ca5db
tjohnson2718 <tjohnson@student.neumont.edu>
2023-11-08 16:22:16 -0700

    removed a couple of things

--- Source/Application/World06.cpp
+++ Source/Application/World06.cpp
@@ -25,42 +25,6 @@ namespace nc
             material->albedoTexture = texture;
         }
 
-        //Light
-       /* {
-            auto actor = CREATE_CLASS(Actor);
-            actor->name = "light1";
-            actor->transform.position = glm::vec3{ 3, 3, 3 };
-            auto lightComponent = CREATE_CLASS(LightComponent);
-            lightComponent->type = LightComponent::eType::Point;
-            lightComponent->color = glm::rgbColor(glm::vec3{ randomf() * 360, 1, 1 });
-            lightComponent->intensity = 1;
-            lightComponent->range = 20;
-            lightComponent->innerAngle = 10.0f;
-            lightComponent->outerAngle = 30.0f;
-            actor->AddComponent(std::move(lightComponent));
-            m_scene->Add(std::move(actor));
-        }*/
-
-        //Camera
-        /*{
-            auto actor = CREATE_CLASS(Actor);
-            actor->name = "camera1";
-            actor->transform.position = glm::vec3{ 0, 0, 3 };
-            actor->transform.rotation = glm::radians(glm::vec3{ 0, 180, 0 });
-
-            auto cameraComponent = CREATE_CLASS(CameraComponent);
-            cameraComponent->SetPerspective(70.0f, ENGINE.GetSystem<Renderer>()->GetWidth() / (float)ENGINE.GetSystem<Renderer>()->GetHeight(), 0.1f, 100.0f);
-            actor->AddComponent(std::move(cameraComponent));
-
-            auto cameraController = CREATE_CLASS(CameraController);
-            cameraController->speed = 5;
-            cameraController->sensitivity = 0.5;
-            cameraController->m_owner = actor.get();
-            cameraController->Initialize();
-            actor->AddComponent(std::move(cameraController));
-
-            m_scene->Add(std::move(actor));
-        }*/
         return true;
     }
 
